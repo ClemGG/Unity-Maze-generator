75ec54c5d90027668b9a01c5153d848f28b96b67
Clement Guichard <c.guichard508@laposte.net>
2022-08-01 16:54:05 +0200

    +houston async gen

--- Assets/Maze/Scripts/Runtime/Generation/Algorithms/Houston.cs
+++ Assets/Maze/Scripts/Runtime/Generation/Algorithms/Houston.cs
@@ -1,5 +1,8 @@
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Project.Procedural.MazeGeneration
 {
@@ -81,5 +84,77 @@ namespace Project.Procedural.MazeGeneration
             }
 
         }
+
+
+        public IEnumerator ExecuteAsync(IGrid grid, IProgress<GenerationProgressReport> progress, Cell start = null)
+        {
+            GenerationProgressReport report = new();
+
+            List<Cell> unvisited = new(grid.Size());
+            foreach (Cell c in grid.EachCell())
+            {
+                if (c is null) continue;
+
+                unvisited.Add(c);
+            }
+
+            Cell current = start ?? unvisited.Sample();
+            unvisited.Remove(current);
+
+            while (unvisited.Count > 0)
+            {
+                if (unvisited.Count / grid.Size() - 1 > _houstonSwapPercent)
+                {
+                    //Aldous-Broder
+                    Cell neighbor = current;
+                    if (neighbor.Links.Count == 0)
+                    {
+                        current.Link(neighbor);
+                    }
+
+                    unvisited.Remove(current);
+                    current = neighbor;
+                }
+                else
+                {
+                    //Houston
+                    Cell cell;
+                    do
+                    {
+                        cell = unvisited.Sample();
+                    }
+                    while (cell is null);
+
+                    List<Cell> path = new() { cell };
+
+                    while (unvisited.Contains(cell))
+                    {
+                        cell = cell.Neighbors.Sample();
+                        int position = path.IndexOf(cell);
+
+                        if (position == -1)
+                        {
+                            path.Add(cell);
+                        }
+                        else
+                        {
+                            path = path.GetRange(0, position + 1);
+                        }
+                    }
+
+                    for (int i = 0; i < path.Count - 1; i++)
+                    {
+                        path[i].Link(path[i + 1]);
+                        unvisited.Remove(path[i]);
+                    }
+                }
+
+                report.ProgressPercentage = (float)((grid.Size() - unvisited.Count) * 100 / grid.Size()) / 100f;
+                report.UpdateTrackTime(Time.deltaTime);
+                progress.Report(report);
+                yield return null;
+            }
+        }
+
     }
 }
