8ce08dc0272550905f7341309211d2a9a77bbb2c
Clement Guichard <c.guichard508@laposte.net>
2022-08-01 17:54:22 +0200

    + aldous broder async gen

--- Assets/Maze/Scripts/Runtime/Generation/Algorithms/AldousBroder.cs
+++ Assets/Maze/Scripts/Runtime/Generation/Algorithms/AldousBroder.cs
@@ -1,4 +1,9 @@
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Project.Procedural.MazeGeneration
 {
     //The Aldous-Broder algorithm starts with a random Cell
@@ -26,6 +31,38 @@ namespace Project.Procedural.MazeGeneration
                 cell = neighbor;
             }
 
+        }
+
+
+
+        public IEnumerator ExecuteAsync(IGrid grid, IProgress<GenerationProgressReport> progress, Cell start = null)
+        {
+            GenerationProgressReport report = new();
+            List<Cell> linkedCells = new();
+
+            Cell cell = start ?? grid.RandomCell();
+            int unvisited = grid.Size() - 1;
+
+            while (unvisited > 0)
+            {
+                Cell neighbor = cell.Neighbors.Sample();
+
+                if (neighbor.Links.Count == 0)
+                {
+                    cell.Link(neighbor);
+                    unvisited--;
+
+                    linkedCells.Add(cell);
+                }
+
+                cell = neighbor;
+
+
+                report.ProgressPercentage = (float)(linkedCells.Count * 100 / grid.Size()) / 100f;
+                report.UpdateTrackTime(Time.deltaTime);
+                progress.Report(report);
+                yield return null;
+            }
         }
     }
 }
